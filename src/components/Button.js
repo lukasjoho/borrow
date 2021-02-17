a44e384c9dfb6108f3680f27bf9918fcfe36ef7e
lukasjoho <social@lukashoppe.com>
2021-02-17 20:53:50 +0100

    Initialisierung der App

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './button.css';
+const Button = () => {
+	return (
+		<button class='title'>
+			<img
+				src='https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg'
+				alt=''
+			/>
+			Sport
+		</button>
+	);
+};
+export default Button;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Team</li>
+				<li>Kontakt</li>
+			</ul>
+		</nav>
+	);
+};
+export default Navbar;
